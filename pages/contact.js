861d19f81a52af3f4facb15d2ce1e32dc32ba187
Shan15Dev <61542805+Shan15Dev@users.noreply.github.com>
2022-03-15 14:42:53 +0100

    Contact us Page created

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+export default function Contact(){
+    return(
+        <div>
+            <h1>Contact us</h1>
+            <input placeholder="Subject"></input>
+            <input placeholder="E-Mail"></input>
+            <textarea placeholder="Message" ></textarea>
+            <button>Submit</button>
+        </div>
+    )
+}
