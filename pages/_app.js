4e2aa72fc32b26813b25d9574f5a176d0a4a1b5a
Shan15Dev <61542805+Shan15Dev@users.noreply.github.com>
2022-03-15 15:38:07 +0100

    Menubar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import "../styles/globals.css";
+import Image from 'next/image'
+import Logo from '../public/shanLogoLight.png'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <h1>Covid Cases in Switzerland</h1>
+      <div className="menu">
+        <h1>Covid Cases in Switzerland</h1>
+        <Image src={Logo} alt="The Logo of Shan" className="logo"/>
+      </div>
       <Component {...pageProps} />
     </div>
   );
--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,15 @@
-export default function Contact(){
-    return(
-        <div>
-            <h1>Contact us</h1>
-            <input placeholder="Subject"></input>
-            <input placeholder="E-Mail"></input>
-            <textarea placeholder="Message" ></textarea>
-            <button>Submit</button>
-        </div>
-    )
+import styles from '../styles/contact.module.css'
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact us</h1>
+      <form className={styles.form}>
+        <input placeholder="Subject"></input>
+        <input placeholder="E-Mail"></input>
+        <textarea placeholder="Message"></textarea>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
